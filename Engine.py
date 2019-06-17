608f02049fbf309ff1172c15366e19f542c89744
Vedant Lodha <ivedantlodha@gmail.com>
2019-06-17 14:59:16 +0530

    map api updated

--- Engine.py
+++ Engine.py
@@ -5,175 +5,57 @@ import requests
 import json
 import urllib
 from urllib.request import urlopen
-import pdb
-# def create_data_model(depot, stations, number_of_vehicles):
-#     URL = ''
-#     api_key = ''
-#     locations = depot + stations
-#     data = {}
-#     data['depot'] = 0
-#     data['number_of_vehicles'] = number_of_vehicles
-#     data['distance_matrix'] = []
-#     rquotient, remainder = divmod(len(locations),100)
-#     #Distance Matrix API accepts maximum 100 elements per request
-#     maximum_elements = 100
-#     number_of_addresses = len(locations)
+
+def create_data_matrix():
+    URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&'
+    dest = ''
+
+    #build the URL
+    locations = get_locations()
+    for coordinates in locations:
+            dest += str(coordinates[0]) + ',' + str(coordinates[1])
+            if coordinates != locations[-1]:
+                dest += '|'
     
+    api_key = 'AIzaSyA0fBuKW5FDEUJFMUCiZAv_zgXAon8gSgI'
+    URL += 'origins=' + dest + '&destinations=' + dest + '&key=' + api_key
 
-def create_data():
-  """Creates the data."""
-  data = {}
-  data['API_key'] = 'AIzaSyA0fBuKW5FDEUJFMUCiZAv_zgXAon8gSgI'
-  data['addresses'] = ['3610+Hacks+Cross+Rd+Memphis+TN', # depot
-                       '1921+Elvis+Presley+Blvd+Memphis+TN',
-                       '149+Union+Avenue+Memphis+TN',
-                       '1034+Audubon+Drive+Memphis+TN',
-                       '1532+Madison+Ave+Memphis+TN',
-                       '706+Union+Ave+Memphis+TN',
-                       '3641+Central+Ave+Memphis+TN',
-                       '926+E+McLemore+Ave+Memphis+TN',
-                       '4339+Park+Ave+Memphis+TN',
-                       '600+Goodwyn+St+Memphis+TN',
-                       '2000+North+Pkwy+Memphis+TN',
-                       '262+Danny+Thomas+Pl+Memphis+TN',
-                       '125+N+Front+St+Memphis+TN',
-                       '5959+Park+Ave+Memphis+TN',
-                       '814+Scott+St+Memphis+TN',
-                       '1005+Tillman+St+Memphis+TN'
-                      ]
-  return data
+    #Http get request
+    response = requests.get(URL)
+    response_json = response.json()
 
-def create_distance_matrix(data):
-  addresses = data["addresses"]
-  API_key = data["API_key"]
-  # Distance Matrix API only accepts 100 elements per request, so get rows in multiple requests.
-  max_elements = 100
-  num_addresses = len(addresses) # 16 in this example.
-  max_rows = max_elements 
-  # num_addresses = q * max_rows + r 
-  q, r = divmod(num_addresses, max_rows)
-  dest_addresses = addresses
-  distance_matrix = []
-  # Send q requests, returning max_rows rows per request.
-  for i in range(q):
-    origin_addresses = addresses[i * max_rows: (i + 1) * max_rows]
-    response = send_request(origin_addresses, dest_addresses, API_key)
-    distance_matrix += build_distance_matrix(response)
+    #computation of distance matrix
+    distance_matrix = []
+    for rows in response_json['rows']:
+        row_list = [rows['elements'][j]['distance']['value'] for j in range(len(rows['elements']))]
+        distance_matrix.append(row_list)
+    
+    #Scaling down distance matrix because of or-tools distance constraints
+    for i in range(len(distance_matrix)):
+        for j in range(len(distance_matrix[i])):
+                distance_matrix[i][j] = int(distance_matrix [i][j] / 100)
+    return distance_matrix
 
-  # Get the remaining remaining r rows, if necessary.
-  if r > 0:
-    origin_addresses = addresses[q * max_rows: q * max_rows + r]
-    response = send_request(origin_addresses, dest_addresses, API_key)
-z    distance_matrix += build_distance_matrix(response)
-  return distance_matrix
-
-def send_request(origin_addresses, dest_addresses, API_key):
-  """ Build and send request for the given origin and destination addresses."""
-  def build_address_str(addresses):
-    # Build a pipe-separated string of addresses
-    address_str = ''
-    for i in range(len(addresses) - 1):
-      address_str += addresses[i] + '|'
-    address_str += addresses[-1]
-    return address_str
-
-  request = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
-  origin_address_str = build_address_str(origin_addresses)
-  dest_address_str = build_address_str(dest_addresses)
-  request = request + '&origins=' + origin_address_str + '&destinations=' + \
-                       dest_address_str + '&key=' + API_key
-  jsonResult = urlopen(request).read()
-  response = json.loads(jsonResult)
-  return response
-
-def build_distance_matrix(response):
-  distance_matrix = []
-  for row in response['rows']:
-    row_list = [row['elements'][j]['distance']['value'] for j in range(len(row['elements']))]
-    distance_matrix.append(row_list)
-  return distance_matrix
-
-
-def create_data_model():
+def get_locations():
+    locations = [(12.9571,77.659439),
+                (12.9353173134382,77.7402628734708),
+                (12.965534,77.5341921),
+                (12.965716,77.5347942),
+                # (12.988413,77.630671),
+                # (12.9777275770552,77.7406826391816),
+                # (12.9781522327987,77.7331734597683),
+                # (12.9542097712404,77.7810297682881),
+                (12.9598,77.6434),
+                (12.9580054,77.6478705),
+                (12.9667323,77.7314212),
+                (12.9603074,77.6411069),
+                (12.9645389,77.4449567)]
+    return locations
+def create_data_model(locations, num_vehicles):
     """Stores the data for the problem."""
     data = {}
-    # data['distance_matrix'] = [
-    #     [
-    #         0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354,
-    #         468, 776, 662
-    #     ],
-    #     [
-    #         548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674,
-    #         1016, 868, 1210
-    #     ],
-    #     [
-    #         776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164,
-    #         1130, 788, 1552, 754
-    #     ],
-    #     [
-    #         696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822,
-    #         1164, 560, 1358
-    #     ],
-    #     [
-    #         582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708,
-    #         1050, 674, 1244
-    #     ],
-    #     [
-    #         274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628,
-    #         514, 1050, 708
-    #     ],
-    #     [
-    #         502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856,
-    #         514, 1278, 480
-    #     ],
-    #     [
-    #         194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320,
-    #         662, 742, 856
-    #     ],
-    #     [
-    #         308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662,
-    #         320, 1084, 514
-    #     ],
-    #     [
-    #         194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388,
-    #         274, 810, 468
-    #     ],
-    #     [
-    #         536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764,
-    #         730, 388, 1152, 354
-    #     ],
-    #     [
-    #         502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114,
-    #         308, 650, 274, 844
-    #     ],
-    #     [
-    #         388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194,
-    #         536, 388, 730
-    #     ],
-    #     [
-    #         354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0,
-    #         342, 422, 536
-    #     ],
-    #     [
-    #         468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536,
-    #         342, 0, 764, 194
-    #     ],
-    #     [
-    #         776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274,
-    #         388, 422, 764, 0, 798
-    #     ],
-    #     [
-    #         662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730,
-    #         536, 194, 798, 0
-    #     ],
-    #     # [0,2,6,9],
-    #     # [2,0,8,4],
-    #     # [6,8,0,3],
-    #     # [9,11,3,0]
-    # ]
-    location_details = create_data()
-    data['distance_matrix'] = create_distance_matrix(location_details)
-    data['num_vehicles'] = 3
+    data['distance_matrix'] = create_data_matrix()
+    data['num_vehicles'] = num_vehicles
     data['depot'] = 0
     return data
 
@@ -208,7 +90,7 @@ def no_empty_routes(data, routing):
             start_var.RemoveValue(node_index)
 
 
-def solver(data):
+def solver(data,no_empty_vehivles=false):
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(
         len(data['distance_matrix']), 
@@ -251,13 +133,12 @@ def solver(data):
     search_parameters.log_search = True
 
 
-
     #Ensure all vehicles serve atleast one station
-    no_empty_routes(data, routing)
+    if no_empty_vehicles:
+        no_empty_routes(data, routing)
 
     # Create a solver
     solution = routing.SolveWithParameters(search_parameters)
-    
     return manager, routing, solution
 
 def test():
@@ -266,7 +147,4 @@ def test():
     if solution :
         print_solution(data, manager, routing, solution)
     print("Solver status: ", routing.status())
-
-
-pdb.set_trace()
 test()
