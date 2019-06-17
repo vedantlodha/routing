7d3e50397297c9c0fd0518b3825857f717519017
Vedant Lodha <ivedantlodha@gmail.com>
2019-06-17 10:53:18 +0530

    distance_matrix_google maps

--- routing.py
+++ routing.py
@@ -82,7 +82,7 @@ def create_data_model():
         # [6,8,0,3],
         # [9,11,3,0]
     ]
-    data['num_vehicles'] = 4
+    data['num_vehicles'] = 
     data['depot'] = 0
     return data
 
@@ -109,10 +109,90 @@ def print_solution(data, manager, routing, solution):
 
 
 
+
+def create_data():
+  """Creates the data."""
+  data = {}
+  data['API_key'] = 'AIzaSyA0fBuKW5FDEUJFMUCiZAv_zgXAon8gSgI'
+  data['addresses'] = ['3610+Hacks+Cross+Rd+Memphis+TN', # depot
+                       '1921+Elvis+Presley+Blvd+Memphis+TN',
+                       '149+Union+Avenue+Memphis+TN',
+                       '1034+Audubon+Drive+Memphis+TN',
+                       '1532+Madison+Ave+Memphis+TN',
+                       '706+Union+Ave+Memphis+TN',
+                       '3641+Central+Ave+Memphis+TN',
+                       '926+E+McLemore+Ave+Memphis+TN',
+                       '4339+Park+Ave+Memphis+TN',
+                       '600+Goodwyn+St+Memphis+TN',
+                       '2000+North+Pkwy+Memphis+TN',
+                       '262+Danny+Thomas+Pl+Memphis+TN',
+                       '125+N+Front+St+Memphis+TN',
+                       '5959+Park+Ave+Memphis+TN',
+                       '814+Scott+St+Memphis+TN',
+                       '1005+Tillman+St+Memphis+TN'
+                      ]
+  return data
+
+def create_distance_matrix(data):
+  addresses = data["addresses"]
+  API_key = data["API_key"]
+  # Distance Matrix API only accepts 100 elements per request, so get rows in multiple requests.
+  max_elements = 100
+  num_addresses = len(addresses) # 16 in this example.
+  # Maximum number of rows that can be computed per request (6 in this example).
+  max_rows = max_elements // num_addresses
+  # num_addresses = q * max_rows + r (q = 2 and r = 4 in this example).
+  q, r = divmod(num_addresses, max_rows)
+  dest_addresses = addresses
+  distance_matrix = []
+  # Send q requests, returning max_rows rows per request.
+  for i in range(q):
+    origin_addresses = addresses[i * max_rows: (i + 1) * max_rows]
+    response = send_request(origin_addresses, dest_addresses, API_key)
+    distance_matrix += build_distance_matrix(response)
+
+  # Get the remaining remaining r rows, if necessary.
+  if r > 0:
+    origin_addresses = addresses[q * max_rows: q * max_rows + r]
+    response = send_request(origin_addresses, dest_addresses, API_key)
+    distance_matrix += build_distance_matrix(response)
+  return distance_matrix
+
+def send_request(origin_addresses, dest_addresses, API_key):
+  """ Build and send request for the given origin and destination addresses."""
+  def build_address_str(addresses):
+    # Build a pipe-separated string of addresses
+    address_str = ''
+    for i in range(len(addresses) - 1):
+      address_str += addresses[i] + '|'
+    address_str += addresses[-1]
+    return address_str
+
+  request = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
+  origin_address_str = build_address_str(origin_addresses)
+  dest_address_str = build_address_str(dest_addresses)
+  request = request + '&origins=' + origin_address_str + '&destinations=' + \
+                       dest_address_str + '&key=' + API_key
+  jsonResult = urllib.urlopen(request).read()
+  response = json.loads(jsonResult)
+  return response
+
+def build_distance_matrix(response):
+  distance_matrix = []
+  for row in response['rows']:
+    row_list = [row['elements'][j]['distance']['value'] for j in range(len(row['elements']))]
+    distance_matrix.append(row_list)
+  return distance_matrix
+
+
+
+
+
 def main():
     """Solve the CVRP problem."""
     # Instantiate the data problem.
-    data = create_data_model()
+    # data = create_data_model()
+
 
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(
